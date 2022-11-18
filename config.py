6cc3fb6cdffd7bd54454dd1e148e1f44f20d264b
andrterrius <110309924+andrterrius@users.noreply.github.com>
2022-11-18 14:29:11 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
 from db import DB
-bot = Bot("5452048104:AAFzrvN_LssXCArh1Qy5AaA76DERbxQ5zn8") #вставить свой токен, который можно получить тут: https://t.me/BotFather
+bot = Bot("") #вставить свой токен, который можно получить тут: https://t.me/BotFather
 db = DB() #экземпляр базы данных
 dp = Dispatcher(bot)
